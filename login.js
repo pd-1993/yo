91c4306205b311adc21088d7489e65368be201a2
pd <honortv24@gmail.com>
2025-07-09 22:39:03 +0800

    Create login.js

---
+++ login.js
@@ -0,0 +1,17 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBcmyqSpFbg5mEW7y60dUZAXQ8nEf8QHis",
+    authDomain: "sample-login-d2b3d.firebaseapp.com",
+    projectId: "sample-login-d2b3d",
+    storageBucket: "sample-login-d2b3d.firebasestorage.app",
+    messagingSenderId: "1082557446832",
+    appId: "1:1082557446832:web:d7285c23db5899a3e0d975"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
